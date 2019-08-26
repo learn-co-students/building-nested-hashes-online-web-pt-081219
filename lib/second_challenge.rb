2a5a040b1bde54d6e5aec608828cc57c62a2c4dc
Alfredo Diaz <mail4alfredo@gmail.com>
2019-08-26 20:24:09 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,18 +1,17 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      :patriach => {}, 
-      :matraich => {},
+epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
       :hero => {},
-      :hero_friends => []},
-    :capulet => {
-      #your key/value pairs here!
-      :patriach => {},
-      :matraich => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
       :heroine => {},
-      :herine_friends => []
-      }}
+      :heroine_friends => []
+   }
+}
 
-  
 end
