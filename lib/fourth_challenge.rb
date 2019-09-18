dcab3a79bb12424f72f29eedb71605928070f385
LUIS J Alvarez Diaz <ldiaz244@gmail.com>
2019-09-18 23:50:16 +0000

    Done.

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,19 +1,29 @@
 def fourth_challenge
   epic_tragedy = {
-   :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
-        #your hashes here!
+  : montague => {
+      : patriarch => {name: "Lord Montague", age: "53"},
+      : matriarch => {name: "Lady Montague", age: "54"},
+      : hero => {name: "Romeo", age: "15", status: "alive"},
+      : hero_friends => [
+        : a name of "Benvolio"
+        an: age of "17" and
+        an: attitude of "worried"
+        a: name of "Mercutio"
+        an age of "18"
+        an attitude of "hot-headed"
       ]
    }, 
-   :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
-        #your hashes here!
+   : capulet => {
+      : patriarch => {name: "Lord Capulet", age: "50"},
+      : matriarch => {name: "Lady Capulet", age: "51"},
+      : heroine => {name: "Juliet", age: "15", status: "alive"},
+      : heroine_friends => [
+        a name of "Steven"
+        an age of "30" and
+        an attitude of "confused"
+        a name of "nurse"
+        an age of "44"
+        an attitude of "worried"
       ]
    }
 }
